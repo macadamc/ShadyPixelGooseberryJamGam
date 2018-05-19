01abdbad036ff768371cc13bee6d559e96423d12
macadamc <macadamchris@gmail.com>
2018-05-19 13:41:47 -0400

    sfx

--- GooseBerryJam/Assets/Scripts/Health.cs
+++ GooseBerryJam/Assets/Scripts/Health.cs
@@ -10,13 +10,45 @@ public class Health : MonoBehaviour
     public UnityEvent OnHurt;
     public UnityEvent OnDeath;
 
+    SpriteRenderer rend;
+    bool flashing;
+
+    private void Awake()
+    {
+        rend = GetComponentInChildren<SpriteRenderer>();
+    }
+
     public void Change(int change)
     {
+        if (flashing && change < 0)
+            return;
+
         value += change;
 
-        if (value <= 0)
+        if(value <= 0)
+        {
             OnDeath.Invoke();
-        else if (change < 0)
+            return;
+        }
+
+        if (change < 0)
+        {
+            StartCoroutine(DamageFlash(2));
             OnHurt.Invoke();
+        }
+    }
+
+    public IEnumerator DamageFlash(int flashes)
+    {
+        flashing = true;
+        for (int i = 0; i < flashes; i++)
+        {
+            rend.enabled = false;
+            yield return new WaitForSeconds(0.1f);
+            rend.enabled = true;
+            yield return new WaitForSeconds(0.1f);
+        }
+        flashing = false;
+        yield return null;
     }
 }
