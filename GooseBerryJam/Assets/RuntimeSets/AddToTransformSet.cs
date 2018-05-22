a2fe059ac657ba882f5f2e6eccc98efdcad4412b
macadamc <macadamchris@gmail.com>
2018-05-21 21:41:58 -0400

    enemy spawn manager update

---
+++ GooseBerryJam/Assets/RuntimeSets/AddToTransformSet.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddToTransformSet : MonoBehaviour
+{
+    public TransformRuntimeSet transformRuntimeSet;
+
+    private void OnEnable()
+    {
+        transformRuntimeSet.Add(transform);
+    }
+
+    private void OnDisable()
+    {
+        transformRuntimeSet.Remove(transform);
+    }
+
+}
