cd0def7e9d39ffe28f301f78b99ee33a15b92670
DoucheBagMIKE <mikemac440@gmail.com>
2018-05-26 17:14:14 -0400

    Floating Points Text

---
+++ GooseBerryJam/Assets/FloatingPoints.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloatingPoints : MonoBehaviour {
+    public float floatSpeed;
+	// Update is called once per frame
+	void Update ()
+    {
+        float newY = transform.position.y + (floatSpeed * Time.deltaTime);
+        transform.position = new Vector3(transform.position.x, newY, transform.position.z);
+	}
+
+}
--- GooseBerryJam/Assets/Scripts/ScoreManager.cs
+++ GooseBerryJam/Assets/Scripts/ScoreManager.cs
@@ -1,21 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 [CreateAssetMenu]
 public class ScoreManager : ScriptableObject {
 
     public IntVariable scoreVar;
     public IntVariable comboVar;
+    public GameObject floatingPointsPrefab;
+    int lastPoints;
 
     public void AddPointsCombo(int value)
     {
         scoreVar.value += (value * (1+comboVar.value));
+        lastPoints = (value * (1 + comboVar.value));
     }
 
     public void AddPointsFlat(int value)
     {
         scoreVar.value += value;
+        lastPoints = value;
     }
 
+    public void CreateFloatingPointsObj(GameObject spawnPosObj)
+    {
+        GameObject ft = Instantiate(floatingPointsPrefab, GameObject.Find("Manager").transform.Find("WorldSpaceCanvas"));
+        ft.transform.position = spawnPosObj.transform.position;
+        ft.GetComponent<Text>().text = lastPoints.ToString();
+    }
 }
