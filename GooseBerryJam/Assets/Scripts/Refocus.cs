f4fc3333dba062e0f947b442c7ffdcd444bb919b
macadamc <macadamchris@gmail.com>
2018-05-27 16:55:42 -0400

    particles and refocus

---
+++ GooseBerryJam/Assets/Scripts/Refocus.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Refocus : MonoBehaviour
+{
+    GameObject lastSelectedObject;
+
+    void Update()
+    {
+        if (EventSystem.current.currentSelectedGameObject == null)
+        {
+            //Debug.Log("Reselecting first input");
+            EventSystem.current.SetSelectedGameObject(lastSelectedObject);
+        }
+        else if(lastSelectedObject != EventSystem.current.currentSelectedGameObject)
+        {
+            lastSelectedObject = EventSystem.current.currentSelectedGameObject;
+        }
+    }
+}
