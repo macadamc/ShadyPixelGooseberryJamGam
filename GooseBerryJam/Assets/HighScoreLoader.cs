b468aa9857ceda71624450f1eb1a68d3fcb27f98
DoucheBagMIKE <mikemac440@gmail.com>
2018-05-27 13:32:55 -0400

    SaveLoad groundwork

---
+++ GooseBerryJam/Assets/HighScoreLoader.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+public class HighScoreLoader : MonoBehaviour
+{
+    public SaveLoadManager slm;
+
+	// Use this for initialization
+	void Start ()
+    {
+        slm.filePath = Application.persistentDataPath + "/HighScore.bin";
+
+        if (slm.data == null)
+            slm.data = new Dictionary<string, int>();
+
+        FileInfo info = new FileInfo(slm.filePath);
+
+        if (info.Exists)
+        {
+            slm.LoadFromFile();
+        }
+	}
+
+    
+}
+
+
+
+
---
+++ GooseBerryJam/Assets/SaveLoadManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+[CreateAssetMenu]
+public class SaveLoadManager : ScriptableObject
+{
+    [HideInInspector]
+    public string filePath;
+
+    public Dictionary<string, int> data;
+
+    public IntVariable highScore;
+
+    public void LoadFromFile()
+    {
+        FileStream stream = File.Open(filePath, FileMode.Open);
+        var formatter = new BinaryFormatter();
+        if (stream.Length != 0)
+            data = formatter.Deserialize(stream) as Dictionary<string, int>;
+        stream.Close();
+
+        if (data.ContainsKey("HighScore"))
+        {
+            highScore.SetValue(data["HighScore"]);
+        }
+
+    }
+
+    public void SaveFile()
+    {
+        bool dirty = false;
+
+        if (data.ContainsKey("HighScore"))
+        {
+            if (highScore.value >= data["HighScore"])
+            {
+                data["HighScore"] = highScore.value;
+                dirty = true;
+            }
+        }
+        else
+        {
+            data.Add("HighScore", highScore.value);
+            dirty = true;
+        }
+
+        if (dirty)
+        {
+            FileStream fs = File.Open(filePath, FileMode.OpenOrCreate);
+            var formatter = new BinaryFormatter();
+            formatter.Serialize(fs, data);
+            fs.Close();
+            Debug.Log("fileSaved");
+        }
+
+    }
+}
